64621674c4b269784111b73ce6830d5b605de76e
Taylor Reed <tatecodes@tutanota.com>
2023-05-27 13:10:19 -0700

    update Packs.js to pull from db
    select/deselect all buttons working.
    Still working on: individual checkboxes (can't uncheck on pack)

--- client/src/components/Packs.js
+++ client/src/components/Packs.js
@@ -1,220 +1,180 @@
 // https://reactjssnippet.com/posts/how-to-check-all-checkbox-in-react-js/
-import React from "react";
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from "react";
 import Axios from 'axios';
 
-export default class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
+export default function PackList() {
+    const [expansionPacks, setExpansionPacks] = useState([]);
+    const [gamePacks, setGamePacks] = useState([]); 
+    const [stuffPacks, setStuffPacks] = useState([]);
+    const [kits, setKits] = useState([]);
 
-
-    this.state = {
-      expansionPacks: [
-        { id: "ep1", label: "Get To Work\n", checked: true },
-        { id: "ep2", label: "Get Together", checked: true },
-        { id: "ep3", label: "City Living", checked: true },
-        { id: "ep4", label: "Cats & Dogs\n", checked: true },
-        { id: "ep5", label: "Seasons\n", checked: true },
-        { id: "ep6", label: "Get Famous", checked: true },
-        { id: "ep7", label: "Island Living", checked: true },
-        { id: "ep8", label: "Discover University", checked: true },
-        { id: "ep9", label: "Eco Lifestyle", checked: true },
-        { id: "ep10", label: "Snowy Escape", checked: true },
-        { id: "ep11", label: "Cottage Living", checked: true },
-        { id: "ep12", label: "High School Years", checked: true },
-        { id: "ep13", label: "Growing Together", checked: true },
-        ],
-      gamePacks: [
-        { id: "gp1", label: "Outdoor Retreat", checked: true },
-        { id: "gp2", label: "Spa Day Pack", checked: true },
-        { id: "gp3", label: "Dine Out Pack", checked: true },
-        { id: "gp4", label: "Vampires Pack", checked: true },
-        { id: "gp5", label: "Parenthood", checked: true },
-        { id: "gp6", label: "Jungle Adventures", checked: true },
-        { id: "gp7", label: "StrangerVille", checked: true },
-        { id: "gp8", label: "Realm of Magic", checked: true },
-        { id: "gp9", label: "Journey to Batuu", checked: true },
-        { id: "gp10", label: "Dream Home Decorator", checked: true },
-        { id: "gp11", label: "My Wedding Stories", checked: true },
-        { id: "gp12", label: "Werewolves", checked: true }
-      ],
-      stuffPacks: [
-        { id: "sp1", label: "Cool Kitchen Stuff", checked: true },
-        { id: "sp2", label: "Spooky Stuff", checked: true },
-        { id: "sp3", label: "Movie Hangout Stuff", checked: true },
-        { id: "sp4", label: "Romantic Garden Stuff", checked: true },
-        { id: "sp5", label: "Kids Room Stuff", checked: true },
-        { id: "sp6", label: "Vintage Glamour Stuff", checked: true },
-        { id: "sp7", label: "Bowling Night Stuff", checked: true },
-        { id: "sp8", label: "Fitness Stuff", checked: true },
-        { id: "sp9", label: "Toddler Stuff", checked: true },
-        { id: "sp10", label: "Laundry Day", checked: true },
-        { id: "sp11", label: "My First Pet Stuff", checked: true },
-        { id: "sp12", label: "Moschino Stuff", checked: true },
-        { id: "sp13", label: "Tiny Living Stuff", checked: true },
-        { id: "sp14", label: "Nifty Knitting Stuff", checked: true },
-        { id: "sp15", label: "Paranormal Stuff", checked: true }
-    ],
-      kits: [
-        { id: "k1", label: "\nBust the Dust Kit", checked: true },
-        { id: "k2", label: "Little Campers Kit", checked: true },
-      ]
-    };
-  }
-
-  selectAll() {
-    const changeAll = this.state.expansionPacks.map((checkbox) => {
-      return { ...checkbox, checked: true };
+  // receive packs from get request
+  useEffect(() => {
+    Axios.get('/api2/packs').then((response) => {
+      const data = response.data;
+        
+      // filters packs by type before mapping
+      const expansionPacksData = data.filter(pack => pack.packType === 'Expansion');
+      const gamePacksData = data.filter(pack => pack.packType === 'Game');
+      const stuffPacksData = data.filter(pack => pack.packType === 'Stuff'); 
+      const kitsData = data.filter(pack => pack.packType === 'Kit');    
+  
+      // map packs to appropriate arrays
+      setExpansionPacks(expansionPacksData.map(pack => ({
+        id: pack.packID,
+        packName: pack.packName,
+        type: pack.packType,
+        checked: pack.selected
+      })));
+  
+      setGamePacks(gamePacksData.map(pack => ({
+        id: pack.packID,
+        packName: pack.packName,
+        type: pack.packType,
+        checked: pack.selected
+      })));
+  
+      setStuffPacks(stuffPacksData.map(pack => ({
+        id: pack.packID,
+        packName: pack.packName,
+        type: pack.packType,
+        checked: pack.selected
+      })));
+  
+      setKits(kitsData.map(pack => ({
+        id: pack.packID,
+        packName: pack.packName,
+        type: pack.packType,
+        checked: pack.selected
+      })));
     });
+  }, []);
 
-    const changeAll2 = this.state.gamePacks.map((checkbox) => {
-        return { ...checkbox, checked: true };
-      });
+
+    // Action of select all button
+    const selectAll = () => {
+        const changeAll = expansionPacks.map((checkbox) => ({
+            ...checkbox, checked: true,
+        }));
+
+        const changeAll2 = gamePacks.map((checkbox) => ({
+            ...checkbox, checked: true,
+        }));
+
+        const changeAll3 = stuffPacks.map((checkbox) => ({
+            ...checkbox, checked: true,
+        }));
+
+        const changeAll4 = kits.map((checkbox) => ({
+            ...checkbox, checked: true,
+        }));
     
-    const changeAll3 = this.state.stuffPacks.map((checkbox) => {
-    return { ...checkbox, checked: true };
-    });
+        setExpansionPacks(changeAll);
+        setGamePacks(changeAll2);
+        setStuffPacks(changeAll3);
+        setKits(changeAll4);
+    };
+    
+    // Action of deselect all button
+    const deselectAll = () => {
+        const changeAll = expansionPacks.map((checkbox) => ({
+            ...checkbox, checked: false,
+        }));
+        
+        const changeAll2 = gamePacks.map((checkbox) => ({
+            ...checkbox, checked: false,
+        }));
 
-    const changeAll4 = this.state.kits.map((checkbox) => {
-        return { ...checkbox, checked: true };
-    });
+        const changeAll3 = stuffPacks.map((checkbox) => ({
+            ...checkbox, checked: false,
+        }));
 
-    this.setState({
-      expansionPacks: changeAll,
-      gamePacks: changeAll2,
-      stuffPacks: changeAll3,
-      kits: changeAll4
-    });
-  }
+        const changeAll4 = kits.map((checkbox) => ({
+            ...checkbox, checked: false,
+        }));
+        
+        setExpansionPacks(changeAll);
+        setGamePacks(changeAll2);
+        setStuffPacks(changeAll3);
+        setKits(changeAll4);
+    };
 
-  deselectAll() {
-    const changeAll = this.state.expansionPacks.map((checkbox) => {
-      return { ...checkbox, checked: false };
-    });
+    // Action of individual checkbox
+    const handleChange = (checkboxId) => {
+        const updateCheckbox = (checkbox) => {
+            if (checkbox.id === checkboxId) {
+                return {...checkbox, checked: !checkbox.checked};
+        }
+        return checkbox;
+    };
 
-    const changeAll2 = this.state.gamePacks.map((checkbox) => {
-        return { ...checkbox, checked: false };
-    });
-
-    const changeAll3 = this.state.stuffPacks.map((checkbox) => {
-        return { ...checkbox, checked: false };
-    });
-
-    const changeAll4 = this.state.kits.map((checkbox) => {
-        return { ...checkbox, checked: false };
-    });
-
-    this.setState({
-        expansionPacks: changeAll,
-        gamePacks: changeAll2,
-        stuffPacks: changeAll3,
-        kits: changeAll4
-    });
-  }
-
-  handleChange(checkboxId) {
-    let expansionPacks = this.state.expansionPacks;
-    for (let checkbox of expansionPacks) {
-      if (checkbox.id === checkboxId) {
-        checkbox.checked = !checkbox.checked;
-      }
-    }
-    this.setState({ expansionPacks: expansionPacks });
-
-    let gamePacks = this.state.gamePacks;
-    for (let checkbox of gamePacks) {
-      if (checkbox.id === checkboxId) {
-        checkbox.checked = !checkbox.checked;
-      }
-    }
-
-    this.setState({ gamePacks: gamePacks });
-
-    let stuffPacks = this.state.stuffPacks;
-    for (let checkbox of stuffPacks) {
-      if (checkbox.id === checkboxId) {
-        checkbox.checked = !checkbox.checked;
-      }
-    }
-    this.setState({ stuffPacks: stuffPacks });
-
-    let kits = this.state.kits;
-    for (let checkbox of kits) {
-      if (checkbox.id === checkboxId) {
-        checkbox.checked = !checkbox.checked;
-      }
-    }
-    this.setState({ kits: kits });
-  }
-
-  render() {
+};
     return (
       <div className="container">
         <div>
-        <label><strong>Expansion Packs</strong><br/></label>
-        {this.state.expansionPacks.map((checkbox) => (
-          <label key={checkbox.id}>
-            <input
-              type="checkbox"
-              checked={checkbox.checked}
-              onChange={() => this.handleChange(checkbox.id)}
-            />
-            {checkbox.label}
-          </label>
-        ))}
-        <br />
-        </div>
-
-        <div>
-        <label><strong>Game Packs</strong><br/></label>
-        {this.state.gamePacks.map((checkbox) => (
-          <label key={checkbox.id}>
-            <input
-              type="checkbox"
-              checked={checkbox.checked}
-              onChange={() => this.handleChange(checkbox.id)}
-            />
-            {checkbox.label}
+          <label><strong>Expansion Packs</strong><br/></label>
+          {expansionPacks.map((checkbox) => (
+            <label key={checkbox.packID}>
+              <input
+                type="checkbox"
+                checked={checkbox.checked}
+                onChange={() => handleChange(checkbox.packID, 'expansionPacks')}
+              />
+              {checkbox.packName}
+            </label>
+          ))}
           <br />
-          </label>
-        ))}
         </div>
-
+  
         <div>
-        <label><strong>Stuff Packs</strong><br/></label>
-        {this.state.stuffPacks.map((checkbox) => (
-          <label key={checkbox.id}>
-            <input
-              type="checkbox"
-              checked={checkbox.checked}
-              onChange={() => this.handleChange(checkbox.id)}
-            />
-            {checkbox.label}
-            <br />
-          </label>
-        ))}
+          <label><strong>Game Packs</strong><br/></label>
+          {gamePacks.map((checkbox) => (
+            <label key={checkbox.packID}>
+              <input
+                type="checkbox"
+                checked={checkbox.checked}
+                onChange={() => handleChange(checkbox.packID, 'gamePacks')}
+              />
+              {checkbox.packName}
+              <br />
+            </label>
+          ))}
         </div>
-
+  
         <div>
-        <label><strong>Kit Packs<br/></strong></label>
-        {this.state.kits.map((checkbox) => (
-          <label key={checkbox.id}>
-            <input
-              type="checkbox"
-              checked={checkbox.checked}
-              onChange={() => this.handleChange(checkbox.id)}
-            />
-            {checkbox.label}
-            <br />
-          </label>
-        ))}
-        <br />
+          <label><strong>Stuff Packs</strong><br/></label>
+          {stuffPacks.map((checkbox) => (
+            <label key={checkbox.packID}>
+              <input
+                type="checkbox"
+                checked={checkbox.checked}
+                onChange={() => handleChange(checkbox.packID, 'stuffPacks')}
+              />
+              {checkbox.packName}
+              <br />
+            </label>
+          ))}
         </div>
-        
+  
+        <div>
+          <label><strong>Kit Packs<br/></strong></label>
+          {kits.map((checkbox) => (
+            <label key={checkbox.packID}>
+              <input
+                type="checkbox"
+                checked={checkbox.checked}
+                onChange={() => handleChange(checkbox.packID, 'kits')}
+              />
+              {checkbox.packName}
+              <br />
+            </label>
+          ))}
+          <br />
+        </div>
+  
         <br />
-        <button type="button" className="btn btn-light" onClick={() => this.selectAll()}>Select all</button>
-        <button type="button" className="btn btn-light" onClick={() => this.deselectAll()}>Deselect All</button>
+        <button type="button" className="btn btn-light" onClick={() => selectAll()}>Select all</button>
+        <button type="button" className="btn btn-light" onClick={() => deselectAll()}>Deselect All</button>
       </div>
     );
   }
-}
