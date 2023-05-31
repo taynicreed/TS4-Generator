49bacc25afa1e9b4e612aa07bd6cc641bc02461e
Taylor Reed <tatecodes@tutanota.com>
2023-05-31 13:01:44 -0700

    Update index.js
    Update query that fetches Rules so they are readable and not IDs. Started implementing queries for unused rules and overwriting previous rules.

--- server/index.js
+++ server/index.js
@@ -17,10 +17,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Get data for rules table
 app.get('/rules', (req, res) => {
-  query1 = 'SELECT * FROM Rules;'
+  query = `SELECT Rules.genID, Family.familyDescription, Aspirations.aspName, 
+          Careers.careerName, Traits.traitName, Skills.skillName, 
+          Misc.miscDescription FROM Rules 
+          INNER JOIN Family ON Rules.familyID = Family.familyID 
+          INNER JOIN Aspirations ON Rules.aspID = Aspirations.aspID 
+          INNER JOIN Careers ON Rules.careerID = Careers.careerID 
+          INNER JOIN Traits ON Rules.traitID = Traits.traitID 
+          INNER JOIN Skills ON Rules.skillID = Skills.skillID
+          INNER JOIN Misc ON Rules.miscID = Misc.miscID;`;
   
-  db.con.query(query1, function(err, results, fields){
-    res.send(results)
+  db.con.query(query, function(err, results, fields){
+    console.log("rules: ", results);
+    res.send(results);
   });
 });
 
@@ -32,6 +41,7 @@ app.get('/packs', (req, res) => {
     res.send(results)
   });
 });
+
 
 // update selected packs
 app.put('/packOptions', (req, res) => {
@@ -76,6 +86,38 @@ app.put('/packOptions', (req, res) => {
       res.status(500).send(err);
     });
 });
+
+// mark rules as unused
+app.put('/unusedRules', (req, res) => {
+  const rulesToUpdate = req.body;
+  //figure out how to map through rulesToUpdate and update each rule
+  // Family
+  // Aspirations
+  // Careers
+  // Traits
+  // Skills
+  // Misc
+  
+});
+
+// overwrite PrevRules with Rules
+app.put('/overwritePrevRules', (req, res) => {
+  const query = `UPDATE PrevRules 
+    INNER JOIN Rules ON PrevRules.genID = Rules.genID
+    SET
+      PrevRules.familyID = Rules.familyID,
+      PrevRules.aspID = Rules.aspID,
+      PrevRules.careerID = Rules.careerID,
+      PrevRules.traitID = Rules.traitID,
+      PrevRules.skillID = Rules.skillID,
+      PrevRules.miscID = Rules.miscID;`;
+  
+  db.con.query(query, function(err, results, fields){
+    console.log("update: ", results);
+    res.send(results);
+  });
+});
+
 
 app.listen(PORT, function(){
   console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
